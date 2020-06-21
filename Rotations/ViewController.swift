4b4b7f123bfbcc9c7b0850e8fec88b753d8726ae
Erika Yardumian <eyard@erikas-mbp>
2020-06-20 20:49:29 -0700

    Initial Commit

---
+++ Rotations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rotations
+//
+//  Created by Erika Yardumian on 6/20/20.
+//  Copyright © 2020 Erika Yardumian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
