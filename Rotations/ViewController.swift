fdfa0e324f22b9884e8736dde5544ebc57cd5236
Erika Yardumian <eyard@erikas-mbp.attlocal.net>
2020-07-21 16:28:42 -0700

    Working Code

--- Rotations/ViewController.swift
+++ Rotations/ViewController.swift
@@ -20,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var numPmts: UITextField!
+    @IBOutlet weak var intRate: UITextField!
+    @IBOutlet weak var loanAmt: UITextField!
+    @IBOutlet weak var payment: UILabel!
+    
+    var numberPmts: String = "";
+    var rate: String = "";
+    var loanAmount: String = "";
+    var pmt: Double = 0.0;
+    var num: Double = 0.0;
+    var den: Double = 0.0;
+    var floatRate: Double = 0.0;
+    
+    @IBAction func calcPmt(_ sender: Any) {
+        loanAmount = loanAmt.text!
+        let loan = (loanAmount as NSString).doubleValue
+        numberPmts = numPmts.text!
+        let floatPayments = (numberPmts as NSString).doubleValue
+        rate = intRate.text!
+        let fr = (rate as NSString).doubleValue
+        floatRate = fr / 100
+        pmt = (loan * (floatRate * (pow(1 + floatRate, floatPayments)))) / (pow(1 + floatRate, floatPayments) - 1 )
+        let final = String(format: "%.2f", pmt)
+        payment.text! = "$" + final
+    }
+    
 }
 
